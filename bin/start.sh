fcf375243e7518952210c4a4140bcb2a403d7c9b
董菲 <feidong@anjuke.com>
2012-10-16 18:12:38 +0800

    change start.sh to choose similary class

--- bin/start.sh
+++ bin/start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mvn exec:java -Dexec.mainClass=com.anjuke.romar.http.jetty.RomarMain -Dexec.args=$1
+mvn exec:java -Dexec.mainClass=com.anjuke.romar.http.jetty.RomarMain -Dsimilarity-classname=org.apache.mahout.cf.taste.impl.similarity.TanimotoCoefficientSimilarity -Dexec.args=$1
