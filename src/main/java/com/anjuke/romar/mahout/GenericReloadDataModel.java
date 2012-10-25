0882e6ea00fc4681a679fda1400014b1d27e4532
张尔宁 <enzhang@anjuke.com>
2012-10-25 13:47:00 +0800

    refactor coding style

--- src/main/java/com/anjuke/romar/mahout/GenericReloadDataModel.java
+++ src/main/java/com/anjuke/romar/mahout/GenericReloadDataModel.java
@@ -20,16 +20,16 @@ import com.anjuke.romar.mahout.util.Util;
 public class GenericReloadDataModel implements PreferenceDataModel {
     private static final long serialVersionUID = -4393051837705770391L;
 
-    private volatile GenericDataModel currentModel;
+    private volatile GenericDataModel _currentModel;
 
-    private final List<Preference> addData;
-    private final List<Preference> removeData;
+    private final List<Preference> _addData;
+    private final List<Preference> _removeData;
 
     public GenericReloadDataModel() {
         super();
-        currentModel = new GenericDataModel(new FastByIDMap<PreferenceArray>());
-        addData = new LinkedList<Preference>();
-        removeData = new LinkedList<Preference>();
+        _currentModel = new GenericDataModel(new FastByIDMap<PreferenceArray>());
+        _addData = new LinkedList<Preference>();
+        _removeData = new LinkedList<Preference>();
     }
 
     /**
@@ -38,135 +38,131 @@ public class GenericReloadDataModel implements PreferenceDataModel {
     @Override
     public void setPreference(long userID, long itemID,
             float preferenceValue) throws TasteException {
-        addData.add(new GenericPreference(userID, itemID, preferenceValue));
+        _addData.add(new GenericPreference(userID, itemID, preferenceValue));
     }
 
     /** See the warning at {@link #setPreference(long, long, float)}. */
     @Override
     public void removePreference(long userID, long itemID)
             throws TasteException {
-        removeData.add(new BooleanPreference(userID, itemID));
+        _removeData.add(new BooleanPreference(userID, itemID));
     }
 
     @Override
     public void refresh(Collection<Refreshable> alreadyRefreshed) {
-        FastByIDMap<PreferenceArray> data = currentModel.getRawUserData()
+        FastByIDMap<PreferenceArray> data = _currentModel.getRawUserData()
                 .clone();
         applyAddData(data);
         applyRemoveData(data);
-        currentModel = new GenericDataModel(data);
+        _currentModel = new GenericDataModel(data);
     }
-
 
     private void applyRemoveData(FastByIDMap<PreferenceArray> data){
-        for (Preference removePreference : removeData) {
+        for (Preference removePreference : _removeData) {
             Util.applyRemove(data, removePreference);
         }
-        removeData.clear();
+        _removeData.clear();
     }
 
-
     private void applyAddData(FastByIDMap<PreferenceArray> data) {
-        for (Preference addPreference : addData) {
+        for (Preference addPreference : _addData) {
             Util.applyAdd(data, addPreference);
         }
-        addData.clear();
+        _addData.clear();
     }
 
     @Override
     public FastByIDMap<PreferenceArray> getRawUserData(){
-        return currentModel.getRawUserData();
+        return _currentModel.getRawUserData();
     }
 
     @Override
     public FastByIDMap<PreferenceArray> getRawItemData() {
-        return currentModel.getRawItemData();
+        return _currentModel.getRawItemData();
     }
-
 
     @Override
     public PreferenceArray getPreferencesFromUser(long userID)
             throws TasteException {
-        return currentModel.getPreferencesFromUser(userID);
+        return _currentModel.getPreferencesFromUser(userID);
     }
 
     @Override
     public FastIDSet getItemIDsFromUser(long userID) throws TasteException {
-        return currentModel.getItemIDsFromUser(userID);
+        return _currentModel.getItemIDsFromUser(userID);
     }
 
     @Override
     public LongPrimitiveIterator getItemIDs() throws TasteException {
-        return currentModel.getItemIDs();
+        return _currentModel.getItemIDs();
     }
 
     @Override
     public PreferenceArray getPreferencesForItem(long itemID)
             throws TasteException {
-        return currentModel.getPreferencesForItem(itemID);
+        return _currentModel.getPreferencesForItem(itemID);
     }
 
     @Override
     public Float getPreferenceValue(long userID, long itemID)
             throws TasteException {
-        return currentModel.getPreferenceValue(userID, itemID);
+        return _currentModel.getPreferenceValue(userID, itemID);
     }
 
     @Override
     public Long getPreferenceTime(long userID, long itemID)
             throws TasteException {
-        return currentModel.getPreferenceTime(userID, itemID);
+        return _currentModel.getPreferenceTime(userID, itemID);
     }
 
     @Override
     public int getNumItems() throws TasteException {
-        return currentModel.getNumItems();
+        return _currentModel.getNumItems();
     }
 
     @Override
     public int getNumUsers() throws TasteException {
-        return currentModel.getNumUsers();
+        return _currentModel.getNumUsers();
     }
 
     @Override
     public int getNumUsersWithPreferenceFor(long itemID) throws TasteException {
-        return currentModel.getNumUsersWithPreferenceFor(itemID);
+        return _currentModel.getNumUsersWithPreferenceFor(itemID);
     }
 
     @Override
     public int getNumUsersWithPreferenceFor(long itemID1, long itemID2)
             throws TasteException {
-        return currentModel.getNumUsersWithPreferenceFor(itemID1, itemID2);
+        return _currentModel.getNumUsersWithPreferenceFor(itemID1, itemID2);
     }
 
     @Override
     public boolean hasPreferenceValues() {
-        return currentModel.hasPreferenceValues();
+        return _currentModel.hasPreferenceValues();
     }
 
     @Override
     public float getMaxPreference() {
-        return currentModel.getMaxPreference();
+        return _currentModel.getMaxPreference();
     }
 
     @Override
     public float getMinPreference() {
-        return currentModel.getMinPreference();
+        return _currentModel.getMinPreference();
     }
 
     @Override
     public LongPrimitiveIterator getUserIDs() throws TasteException {
-        return currentModel.getUserIDs();
+        return _currentModel.getUserIDs();
     }
 
     @Override
     public void reload(FastByIDMap<PreferenceArray> data) {
-         currentModel = new GenericDataModel(data);
+         _currentModel = new GenericDataModel(data);
     }
 
     @Override
     public void compact() {
         throw new UnsupportedOperationException();
     }
-
 }
